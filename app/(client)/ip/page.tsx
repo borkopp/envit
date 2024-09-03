3bb71e2a7f8a9b4ee26b063cfe2896d8345bb08c
Borko Petrevski <borko.petrevski@hs-augsburg.de>
2024-09-03 16:46:28 +0200

    fix errors

--- app/(client)/ip/page.tsx
+++ app/(client)/ip/page.tsx
@@ -1,46 +1,60 @@
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
 import { FaFilePdf } from "react-icons/fa6";
 import Link from "next/link";
-import Image from "next/image";
 
 export default function Ip() {
   return (
     <div className="container mx-auto px-6 md:px-16 lg:px-24 py-10">
-      
       {/* Paragraph */}
       <p className="text-lg mb-8">
-      It is prior knowledge that EDTA-based soil-washing efficiently removes Pb and other toxic metals from soil. However, more than 20-years of research by many groups worldwide has not resulted in a feasible solution for EDTA recycling, for the problem of vast amounts of generated waste waters and for toxic emission due to EDTA environmental persistence. ReSoil resolves all these problems. It is an internationally patented, cost-efficient, soil preserving and environmentally safe remediation technology.
-</p>
+        It is prior knowledge that EDTA-based soil-washing efficiently removes
+        Pb and other toxic metals from soil. However, more than 20-years of
+        research by many groups worldwide has not resulted in a feasible
+        solution for EDTA recycling, for the problem of vast amounts of
+        generated waste waters and for toxic emission due to EDTA environmental
+        persistence. ReSoil resolves all these problems. It is an
+        internationally patented, cost-efficient, soil preserving and
+        environmentally safe remediation technology.
+      </p>
 
       {/* Additional Paragraph and Patents */}
       <div className="mb-12">
-
-        <h3 className="text-lg font-semibold mb-2">Patents and Patent Applications:</h3>
+        <h3 className="text-lg font-semibold mb-2">
+          Patents and Patent Applications:
+        </h3>
         <div className="mb-8">
           <ul className="list-disc list-inside mb-4">
             <li>
-              <Link href="https://patents.google.com/patent/US9108233B2" className="text-blue-500 hover:underline">
+              <Link
+                href="https://patents.google.com/patent/US9108233B2"
+                className="text-blue-500 hover:underline"
+              >
                 Washing of contaminated soils, US 9,108,233 B2
               </Link>
             </li>
             <li>
-              <Link href="https://patents.google.com/patent/EP3153246B1" className="text-blue-500 hover:underline">
+              <Link
+                href="https://patents.google.com/patent/EP3153246B1"
+                className="text-blue-500 hover:underline"
+              >
                 Method for soil and sediment remediation, EP 3153246 B1
               </Link>
             </li>
             <li>
-              <Link href="https://patents.google.com/patent/GB201720126D0" className="text-blue-500 hover:underline">
-                Curbing toxic emissions from remediated substrate, GB 201720126 D0
+              <Link
+                href="https://patents.google.com/patent/GB201720126D0"
+                className="text-blue-500 hover:underline"
+              >
+                Curbing toxic emissions from remediated substrate, GB 201720126
+                D0
               </Link>
             </li>
             <li>
-              <Link href="https://patents.google.com/patent/WO2022184903A1" className="text-blue-500 hover:underline">
-                Removal of arsenic, antimony and toxic metals from contaminated substrate, WO2022184903A1
+              <Link
+                href="https://patents.google.com/patent/WO2022184903A1"
+                className="text-blue-500 hover:underline"
+              >
+                Removal of arsenic, antimony and toxic metals from contaminated
+                substrate, WO2022184903A1
               </Link>
             </li>
           </ul>
--- app/(client)/soil-washing/page.tsx
+++ app/(client)/soil-washing/page.tsx
@@ -1,26 +1,22 @@
 // Mark this file as a Client Component
 "use client";
 
-import { useState } from 'react';
-import Image from 'next/image';
+import { useState } from "react";
+import Image from "next/image";
 
 export default function SoilWashing() {
   const [selectedImage, setSelectedImage] = useState(null);
 
   const images = [
-    "/washing/3.jpeg",,
-    "/washing/2.jpeg",
     "/washing/3.jpeg",
     "/washing/2.jpeg",
     "/washing/3.jpeg",
     "/washing/2.jpeg",
     "/washing/3.jpeg",
-    "/washing/2.jpeg"
+    "/washing/2.jpeg",
+    "/washing/3.jpeg",
+    "/washing/2.jpeg",
   ];
-
-  const handleImageClick = (src) => {
-    setSelectedImage(src);
-  };
 
   const closeModal = () => {
     setSelectedImage(null);
@@ -31,26 +27,58 @@ export default function SoilWashing() {
       {/* Two Paragraphs with Gap */}
       <div className="mb-12">
         <p className="text-lg mb-6">
-        The soil-washing plant with ReSoil® technology was constructed in city of Prevalje which sits in a contaminated area of Meža Valley, Slovenia. The mission of this facility is to prove the scalability of ReSoil® technology. With the capacity of 6 t/day it enables demonstrational remediation of local sites as well as demonstration of ReSoil® technology to stakeholders. The Technology Readiness Level of plant operation is TRL 7 (EU, NASA methodology).         </p>
+          The soil-washing plant with ReSoil® technology was constructed in
+          city of Prevalje which sits in a contaminated area of Meža Valley,
+          Slovenia. The mission of this facility is to prove the scalability of
+          ReSoil® technology. With the capacity of 6 t/day it enables
+          demonstrational remediation of local sites as well as demonstration of
+          ReSoil® technology to stakeholders. The Technology Readiness Level of
+          plant operation is TRL 7 (EU, NASA methodology).{" "}
+        </p>
         <p className="text-lg">
-        The permit for construction of this soil washing plant was granted by Slovenian Environmental Protection Agency in 2017. The permit was based on proofs of ReSoil® closed process cycles operation and zero-emissions. Construction of the plant was co-financed from EU LIFE + Programme with 50% contribution of 1.1 M EUR. The operating permit for this soil-washing plant was obtained in July 2018. The ReSoil® technology uses common process equipment such are sieves, mixing vessels and filter presses. The remediation process can be controlled manually and the innovative part of ReSoil® technology, i.e. in press soil rinsing, EDTA and process water recycling, can also be controlled automatically by the SCADA (supervisory, control and data acquisition) system.        </p>
+          The permit for construction of this soil washing plant was granted by
+          Slovenian Environmental Protection Agency in 2017. The permit was
+          based on proofs of ReSoil® closed process cycles operation and
+          zero-emissions. Construction of the plant was co-financed from EU LIFE
+          + Programme with 50% contribution of 1.1 M EUR. The operating permit
+          for this soil-washing plant was obtained in July 2018. The ReSoil®
+          technology uses common process equipment such are sieves, mixing
+          vessels and filter presses. The remediation process can be controlled
+          manually and the innovative part of ReSoil® technology, i.e. in press
+          soil rinsing, EDTA and process water recycling, can also be controlled
+          automatically by the SCADA (supervisory, control and data acquisition)
+          system.{" "}
+        </p>
       </div>
 
       {/* Grid of Images */}
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         {images.map((src, index) => (
-          <div key={index} className="relative cursor-pointer" onClick={() => handleImageClick(src)}>
-            <Image src={src} alt={`Image ${index + 1}`} layout="responsive" width={500} height={500} className="object-cover" />
-          </div>
+          <div key={index} className="relative cursor-pointer"></div>
         ))}
       </div>
 
       {/* Modal for Maximized Image */}
       {selectedImage && (
-        <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50" onClick={closeModal}>
+        <div
+          className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
+          onClick={closeModal}
+        >
           <div className="relative bg-white p-4">
-            <Image src={selectedImage} alt="Selected Image" layout="intrinsic" width={1000} height={1000} className="object-contain" />
-            <button className="absolute top-2 right-2 text-white text-2xl font-bold" onClick={closeModal}>X</button>
+            <Image
+              src={selectedImage}
+              alt="Selected Image"
+              layout="intrinsic"
+              width={1000}
+              height={1000}
+              className="object-contain"
+            />
+            <button
+              className="absolute top-2 right-2 text-white text-2xl font-bold"
+              onClick={closeModal}
+            >
+              X
+            </button>
           </div>
         </div>
       )}
