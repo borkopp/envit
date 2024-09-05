d04153915ccec8ce68807d2940372174f917d669
Borko Petrevski <borko.petrevski@hs-augsburg.de>
2024-09-05 15:31:33 +0200

    replaced case studies dummy data on homepage

--- app/components/CaseStudies.tsx
+++ app/components/CaseStudies.tsx
@@ -1,33 +1,49 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { client } from "@/sanity/lib/client";
 
-const CaseStudyItem = ({
-  image,
-  date,
-  title,
-  content,
-}: {
-  image: string;
-  date: string;
+interface CaseStudy {
   title: string;
-  content: string;
-}) => (
+  slug: string;
+  description: string;
+  thumbnail: {
+    asset: {
+      url: string;
+    };
+  };
+  date: string;
+  primaryLink: {
+    type: string;
+    url?: string;
+    file?: {
+      asset: {
+        url: string;
+      };
+    };
+  };
+}
+
+const CaseStudyItem = ({ study }: { study: CaseStudy }) => (
   <div className="w-full md:w-1/2 lg:w-1/4 px-4 mb-8">
     <Image
       className="w-full h-48 object-cover mb-4"
-      src={image}
-      alt={title}
+      src={study.thumbnail.asset.url}
+      alt={study.title}
       width={400}
       height={200}
     />
     <p className="text-white bg-gray-800 px-4 py-2 text-sm w-fit mb-2">
-      {date}
+      {study.date}
     </p>
-    <h4 className="text-xl text-black font-semibold mb-2">{title}</h4>
-    <p className="text-gray-700 mb-4">{content}</p>
+    <h4 className="text-xl text-black font-semibold mb-2">{study.title}</h4>
+    <p className="text-gray-700 mb-4">{study.description}</p>
     <Link
-      href="#"
+      href="/case-studies"
       className="inline-block bg-green-500 text-white py-2 px-4 rounded-full text-sm font-semibold hover:bg-green-600 transition duration-300"
+      target="_blank"
+      rel="noopener noreferrer"
     >
       View Details
     </Link>
@@ -35,36 +51,28 @@ const CaseStudyItem = ({
 );
 
 const CaseStudies = () => {
-  const caseStudies = [
-    {
-      image: "/blog1.jpeg",
-      date: "Nov 2017",
-      title: "Remediation case #1",
-      content:
-        "Construction of vegetable garden with raised beds with remediated soil from City of Prevalje, Meza Valley, Slovenia",
-    },
-    {
-      image: "/blog2.jpeg",
-      date: "Aug 2018",
-      title: "Remediation case #2",
-      content:
-        "Construction of vegetable garden with raised beds with remediated soil from city of Arnoldstein, Austria",
-    },
-    {
-      image: "/blog3.png",
-      date: "Oct 2021",
-      title: "Remediation case #3",
-      content:
-        "Z1-3196 Postdoc project: Remediation of soil contaminated with toxic metals, metalloids and organic pollutants",
-    },
-    {
-      image: "/blog4.jpeg",
-      date: "Jan 2022",
-      title: "Remediation case #4",
-      content:
-        "Finished after Phase 2 Z1-3196 Postdoc project: Remediation of soil contaminated with toxic metals, metalloids and organic pollutants",
-    },
-  ];
+  const [caseStudies, setCaseStudies] = useState<CaseStudy[]>([]);
+
+  useEffect(() => {
+    const fetchCaseStudies = async () => {
+      const query = `*[_type == "caseStudy"] | order(_createdAt desc) [0...4] {
+        title,
+        slug,
+        description,
+        thumbnail {
+          asset-> {
+            url
+          }
+        },
+        date,
+        primaryLink
+      }`;
+      const result = await client.fetch(query);
+      setCaseStudies(result);
+    };
+
+    fetchCaseStudies();
+  }, []);
 
   return (
     <section className="py-16 bg-white" id="case-studies">
@@ -74,13 +82,14 @@ const CaseStudies = () => {
             Case Studies
           </h1>
           <p className="text-gray-600 max-w-2xl mx-auto">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Discover our success stories through these case studies. Each one
+            highlights our innovative solutions and the real-world impact
+            we&apos;ve made for our clients.
           </p>
         </div>
         <div className="flex flex-wrap -mx-4">
           {caseStudies.map((study, index) => (
-            <CaseStudyItem key={index} {...study} />
+            <CaseStudyItem key={index} study={study} />
           ))}
         </div>
       </div>
