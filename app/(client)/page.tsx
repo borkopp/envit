d3692c5ac3ed61ceec1dabea63984278e29cb3c0
Borko Petrevski <borko.petrevski@hs-augsburg.de>
2024-09-07 20:11:27 +0200

    added animation to mobile navbar

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,14 +1,11 @@
 import Head from "next/head";
-import Navbar from "@/app/components/Navbar";
 import Hero from "@/app/components/Hero";
 import LatestNews from "@/app/components/LatestNews";
 import DemoSoilPlant from "@/app/components/DemoSoilPlant";
 import Expertise from "@/app/components/Expertise";
-import Services from "@/app/components/Services";
 import Team from "@/app/components/Team";
 import Testimonials from "@/app/components/Testimonials";
 import CaseStudies from "@/app/components/CaseStudies";
-import Footer from "@/app/components/Footer";
 
 export default function Home() {
   return (
@@ -24,7 +21,6 @@ export default function Home() {
         <LatestNews />
         <DemoSoilPlant />
         <Expertise />
-        {/* <Services /> */}
         <Team />
         <Testimonials />
         <CaseStudies />
